0d791b799e38795d7226e956ca419af9d53c423f
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2023-01-04 14:49:51 +0100

    Minor issues fixed

--- Export_Calendar.php
+++ Export_Calendar.php
@@ -1,44 +1,56 @@
 <?php
 /* Declaring the namespace of the class. */
+
 namespace ECalendar;
 
-class ECalendar{
-    /* Declaring a public property of the class called events, which is an array. */
+class ECalendar
+{
+    
+    /* Declaring the properties of the class. */
     public array $events;
+    public string $name;
 
     /**
-     * The function takes an array of events and assigns it to the events property of the class
+     * The function takes in two parameters,  and , and sets the  property to the
+     *  parameter and the  property to the  parameter
      * 
-     * @param events The events that the listener is listening for.
+     * @param name The name of the event.
+     * @param events An array of events that the listener is listening for.
      */
-    public function __construct($events){
+    public function __construct($name,$events)
+    {
         $this->events = $events;
+        $this->name = $name;
     }
 
     /**
      * The function takes an array of events, and creates an iCalendar file from them
      */
-    public function exportCalendar(){
+    public function exportCalendar()
+    {
         $ical = "BEGIN:VCALENDAR\n";
         $ical .= "VERSION:2.0\n";
-        $ical .= "PRODID:-//LearnPHP.co//NONSGML".$this->name."//EN\n";
+        $ical .= "PRODID:-//LearnPHP.co//NONSGML" . $this->name . "//EN\n";
         $ical .= "METHOD: REQUEST\n";
-        foreach ($events as $event){
-            $slug = strtolower((str_replace(array(' ',"'",'.'),array('_','',''),$this->name)));
+        foreach ($this->events as $event) {
+            $slug = strtolower((str_replace(array(' ', "'", '.'), array('_', '', ''), $this->name)));
             $ical .= "BEGIN:VEVENT\n";
-            $ical .= "uID:".date ('Y-m-d') . 'T' .date ("H:i:s") ."-" . rand () ."-learnphp.co\n";
-            $ical .= "DESCRIPTION:" . $event['description'] . "\n";
-            $ical .= "LOCATION:" . $event['location'] . "\n";
-            $ical .= "DTSTAMP:" .date ('Y-m-d') . 'T' .date ("H:i:s") . "\n";
-            $ical .= "DTSTART:".$event['date_start']." \n";
-            $ical .= "DTEND:".$event['date_end']." \n";
-            $ical .= "SUMMARY: ".$event['name']."\n";
-            $ical .= "DESCRIPTION:".$event['summary']."\n";
+            $ical .= "uID:" . date('Y-m-d') . 'T' . date("H:i:s") . "-" . rand() . "-learnphp.co\n";
+            if (isset($event['description'])) {
+                $ical .= "DESCRIPTION:" . $event['description'] . "\n";
+            }
+            if (isset($event['location'])) {
+                $ical .= "LOCATION:" . $event['location'] . "\n";
+            }
+            $ical .= "DTSTAMP:" . date('Y-m-d') . 'T' . date("H:i:s") . "\n";
+            $ical .= "DTSTART:" . $event['date_start'] . " \n";
+            $ical .= "DTEND:" . $event['date_end'] . " \n";
+            $ical .= "SUMMARY: " . $event['name'] . "\n";
             $ical .= "END:VEVENT\n";
         }
         $ical .= "END: VCALENDAR\n";
         header("Content-Type: text/Calendar; charset=utf-8");
-        header("Content-Disposition: inline; filename=".$slug.".ics");
+        header("Content-Disposition: inline; filename=" . $slug . ".ics");
         echo $ical;
     }
 }
